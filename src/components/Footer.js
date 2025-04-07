5317e5dec1e6a36685737127cb08c79e3fbe78c6
VyomeshPandita <panditavyomesh@gmail.com>
2025-04-07 19:56:37 +0530

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../styles/footer.css";
-function Footer() {
-return <footer className="footer">© 2025 PAYTM WB |
-All Rights Reserved</footer>;
-}
-export default Footer;
